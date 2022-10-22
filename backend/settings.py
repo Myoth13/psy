a096b03aca6038090544d3e232fda4e4dcf8405d
myoth13 <myoth13@gmail.com>
2022-10-22 11:30:13 +0400

    deployment static issue

--- backend/settings.py
+++ backend/settings.py
@@ -205,10 +205,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_collected')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_DIRS = [BASE_DIR/'static']
+STATICFILES_DIRS = [
+    BASE_DIR/'static',
+    os.path.join(BASE_DIR, 'backend/static'),
+]
 
 # Media files
 # DEFAULT_FILE_STORAGE is configured using DEFAULT_STORAGE_DSN
