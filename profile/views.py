f2fbdac10783ebb382db9ec52391564e794a1e52
myoth13 <myoth13@gmail.com>
2022-10-24 14:44:21 +0400

    profile app

--- profile/views.py
+++ profile/views.py
@@ -3,7 +3,8 @@ from .models import UserProfile
 
 
 # Create your views here.
-def profile_main(request, user_id):
+def profile_main(request):
+    user_id = request.user
     user_profile = UserProfile.objects.get(user_id=user_id)
     context = {'profile': user_profile}
     return render(request, 'profile_main.html', context=context)
