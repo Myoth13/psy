046cadd9fcaf140039d3a2a3c0a3df8f23d1bdc1
myoth13 <myoth13@gmail.com>
2022-10-26 08:11:14 +0400

    user profile app

--- profile/views.py
+++ profile/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from .models import UserProfile
 from .forms import UpdateUserProfileForm
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
+@login_required()
 def profile_main(request):
     user_id = request.user
     user_profile = UserProfile.objects.get(user_id=user_id)
@@ -11,10 +14,19 @@ def profile_main(request):
     return render(request, 'profile_main.html', context=context)
 
 
+@login_required()
 def profile_update(request):
     user_id = request.user
     user_profile = UserProfile.objects.get(user_id=user_id)
     form = UpdateUserProfileForm(instance=user_profile)
-    context = {'form': form}
+
+    if request.method == 'POST':
+        form = UpdateUserProfileForm(request.POST, request.FILES, instance=user_profile)
+        if form.is_valid():
+            form.save()
+            messages.info(request, 'Profile was updated successfully')
+            return redirect('profile_main')
+
+    context = {'form': form, 'profile': user_profile}
     return render(request, 'profile_update.html', context=context)
 
